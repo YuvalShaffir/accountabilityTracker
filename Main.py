b155756706ee8a9cc3add36116799ed524e7b108
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2023-12-01 19:17:55 +0200

    Extracted the 'visit_duration' column from the 'visits' table which is in microseconds.
    Transformed it into normal Hour-Min-Second time format.

--- Main.py
+++ Main.py
@@ -1,12 +1,24 @@
 import sqlite3
 import shutil
+import time
 
 shutil.copy('C:\\Users\\yshaf\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History', 'C:\\Users\\yshaf\\PycharmProjects\\Acountability-Tracker')
 con = sqlite3.connect('C:\\Users\\yshaf\\PycharmProjects\\Acountability-Tracker\\History')
 cursor = con.cursor()
-cursor.execute("SELECT url FROM urls")
-urls = cursor.fetchall()
-# Access the first element of each tuple before joining
-formatted_urls = '\n'.join(url[0] for url in urls)
+# Calculate the timestamp for the start of the last day (24 hours ago)
+start_of_last_day = int(time.time()) - 24 * 60 * 60
 
-print(formatted_urls)
+# Execute the SQL query to select search history from the last day
+cursor.execute("SELECT urls.url, visits.visit_duration FROM urls, visits WHERE urls.id = visits.url AND urls.last_visit_time >= ?", (start_of_last_day,))
+
+results = cursor.fetchall()
+
+# Calculate and print the amount of time spent on each site
+for url, visit_duration in results:
+    # Convert microseconds to seconds
+    time_duration_seconds = visit_duration / 1e6
+
+    # Format the time duration in hours, minutes, and seconds
+    formatted_time = time.strftime('%H:%M:%S', time.gmtime(time_duration_seconds))
+
+    print(f"URL: {url}\nTime Spent: {formatted_time}\n")
