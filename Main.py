b75f156b85365815efa76d5c3dc17bce1e42a6a1
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2024-01-03 14:26:14 +0200

    separated and organized the get_url function.

--- Main.py
+++ Main.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 
 # The default Chrome profile name, if you have multiple profiles,
 # change this to the name of the profile you want to use.
+DEFAULT_TIME_THRESHOLD = 1000000
 CHROME_PROFILE = 'Default'
+DEST_FILE_NAME = 'History'
 
 
 def get_history_file(profile=CHROME_PROFILE):
@@ -40,14 +42,22 @@ def get_dest_path():
         raise Exception("Destination path does not exist")
 
 
-def get_urls():
-    url_dict = {}  # Dictionary of URLs and the time spent on each website {URL: Time Spent}
+def connect_history_file():
+    """Copies the Chrome history file to the destination path (helps to override the premission block of the file).
+    Then connects to the database and returns the cursor."""
+    # copy the file to the destination path
     source_path = get_history_file()
     destination_path = get_dest_path()
-
     shutil.copy(source_path, destination_path)
-    con = sqlite3.connect('E:\\PythonProjects\\Acountability-Tracker\\History')
+
+    # connect to the database
+    con = sqlite3.connect(destination_path + DEST_FILE_NAME)
     cursor = con.cursor()
+    return cursor
+
+
+def search_database(cursor):
+    """ Execute the SQL query to select search history from the last day """
     # Calculate the timestamp for the start of the last day (24 hours ago)
     start_of_last_day = int(time.time()) - 24 * 60 * 60
 
@@ -57,25 +67,62 @@ def get_urls():
         (start_of_last_day,))
 
     results = cursor.fetchall()
-    # print(results)
-    # Calculate and print the amount of time spent on each site
+    return results
+
+
+def clean_url(url):
+    """ Remove the query and fragment parts of the URL"""
+    url = urlparse(url)._replace(path='')._replace(fragment="")._replace(query='').geturl()
+    return url
+
+
+def get_formatted_time(visit_duration):
+    """Calculate the formatted time of type Hours:Minutes:Seconds"""
+    # Convert microseconds to seconds
+    time_duration_seconds = visit_duration / 1e6
+    # Format the time duration in hours, minutes, and seconds
+    formatted_time = time.strftime('%H:%M:%S', time.gmtime(time_duration_seconds))
+    return formatted_time
+
+
+def filter_small_duration(url_dict, threshold_time):
+    """Filter the URLs with time spent less than threshold time in microseconds"""
+    url_dict_min_time = {u: v for u, v in url_dict.items() if v > threshold_time}
+    return url_dict_min_time
+
+
+def fill_url_dict(results):
+    """Fill the dictionary with URLs and the time spent on each website {URL: Time Spent}"""
+    url_dict = {}  # Dictionary of URLs and the time spent on each website {URL: Time Spent}
     for url, visit_duration in results:
-        # add to the list
-        url = urlparse(url)._replace(path='')._replace(fragment="")._replace(query='').geturl()
+        url = clean_url(url)
+
         if url not in url_dict:
             url_dict[str(url)] = visit_duration
         else:
             url_dict[str(url)] += visit_duration
 
-    url_dict_min_time = {u:v for u, v in url_dict.items() if v > 1000000000}
+    return url_dict
+
+
+def print_url_dict(url_dict_min_time):
     for url, visit_duration in url_dict_min_time.items():
-
-        # Convert microseconds to seconds
-        time_duration_seconds = visit_duration / 1e6
-
-        # Format the time duration in hours, minutes, and seconds
-        formatted_time = time.strftime('%H:%M:%S', time.gmtime(time_duration_seconds))
+        formatted_time = get_formatted_time(visit_duration)
         print(f"URL: {url}\nTime Spent: {formatted_time}\n")
+
+
+def get_urls():
+    """Returns a dictionary of URLs and the time spent on each website {URL: Time Spent}  from the history of chrome."""
+    cursor = connect_history_file()
+
+    results = search_database(cursor)
+
+    url_dict = fill_url_dict(results)
+
+    # Filter the URLs with time spent less than threshold time in microseconds.
+    url_dict_min_time = filter_small_duration(url_dict, DEFAULT_TIME_THRESHOLD)
+
+    print_url_dict(url_dict_min_time)
 
     return url_dict_min_time
 
