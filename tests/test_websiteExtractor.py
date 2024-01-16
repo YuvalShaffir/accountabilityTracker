515816c8f82607a205daf503eee7941c5a229b15
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2024-01-16 15:52:58 +0200

    solved the error with the tester

--- tests/test_websiteExtractor.py
+++ tests/test_websiteExtractor.py
@@ -1,12 +1,12 @@
-from ...Acountability-Tracker.Scrapper import metaExtractor
+from Scrapper.metaExtractor import metaExtractor
 import unittest
-
+import asyncio
 
 class TestMetaExtractor(unittest.TestCase):
     def test_get_website_content(self):
-        url_dict = {"https://www.cnn.com": 1.33}  # Assuming you have a value associated with the URL
+        url_dict = {"https://edition.cnn.com/": 1.33}  # Assuming you have a value associated with the URL
         meta_extractor = metaExtractor(url_dict)
-
+        print(asyncio.run(meta_extractor.extract()))
         # Your test logic here
 
 if __name__ == '__main__':
