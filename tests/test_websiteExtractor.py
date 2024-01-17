fe32e3fa0e26f52b002bd876ce6c32f4968f4c13
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2024-01-17 10:00:29 +0200

    test_forbidden_access_website_content

--- tests/test_websiteExtractor.py
+++ tests/test_websiteExtractor.py
@@ -4,13 +4,10 @@ import asyncio
 
 
 class TestMetaExtractor(unittest.TestCase):
-    def test_get_website_content(self):
-        url_dict = {"https://en.wikipedia.org/wiki/Stack_Overflow": 1.33}  # Assuming you have a value associated with the URL
+    def test_forbidden_access_website_content(self):
         url_dict = {"https://stackoverflow.com/": 1.33}  # Assuming you have a value associated with the URL
-
         meta_extractor = metaExtractor(url_dict)
         print((meta_extractor.extract()))
-        # Your test logic here
 
 
 if __name__ == '__main__':
