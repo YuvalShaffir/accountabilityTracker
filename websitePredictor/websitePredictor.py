2baef13e63043c54b917f034ce51da45bcf1237b
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2024-01-29 15:41:33 +0200

    make a function that gets the predictor credentials

--- websitePredictor/websitePredictor.py
+++ websitePredictor/websitePredictor.py
@@ -2,31 +2,34 @@ import os
 from google.cloud import language_v1
 from google.cloud import language
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = ('C:\\Users\\User\\Documents\\Google Cloud Service\\'
-                                                'banded-coder-408816-183df3de095c.json')
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = ('C:\\Users\\User\\Documents\\Google Cloud Service\\'
+#                                                 'banded-coder-408816-183df3de095c.json')
 
-# translation = 'Ynet - News, Economy, Sports and Health - Regular reports from Israel and the world the leading news site in Israel from Yedioth Ahronoth.Full coverage of news from Israel and the world, sports, economics, culture, food, science and nature, everything that is happening and everything interesting in Ynet Iran: a senior commander of the Revolutionary Guards is eliminated in Syria Netanyahu: "Military pressure";Hooded Families: "No Time" Hamas: We will not give up control for a ceasefire and Ariel Bibs 80 days in Hamas captivity: "Bad Dream" also under missiles: the factory in Sderot not stopped'
+gcs_folder = os.path.expanduser('~\Documents\Google Cloud Services')
+json_file = [pos_json for pos_json in os.listdir(gcs_folder) if pos_json.endswith('.json')]
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.expanduser(gcs_folder + "/" + json_file[0])
+print(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+translation = 'Ynet - News, Economy, Sports and Health - Regular reports from Israel and the world the leading news site in Israel from Yedioth Ahronoth.Full coverage of news from Israel and the world, sports, economics, culture, food, science and nature, everything that is happening and everything interesting in Ynet Iran: a senior commander of the Revolutionary Guards is eliminated in Syria Netanyahu: "Military pressure";Hooded Families: "No Time" Hamas: We will not give up control for a ceasefire and Ariel Bibs 80 days in Hamas captivity: "Bad Dream" also under missiles: the factory in Sderot not stopped'
 # print(f"Website Metadata: {translation}\n")
 
 
 class websitePredictor:
+    client_ = language_v1.LanguageServiceClient()
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+    language_ = "en"
+
     def __init__(self, metadata_dict):
         self._meta_dict = metadata_dict
 
-    @staticmethod
-    def _get_prediction(metadata):
+    def _get_prediction(self, metadata):
         try:
             text_content = str(metadata)[0:1000]
 
-            client = language_v1.LanguageServiceClient()
-
-            type_ = language_v1.Document.Type.PLAIN_TEXT
-            language_ = "en"
-            document_dict = {"type": type_, "language": language_, "content": text_content}
+            document_dict = {"type": self.type_, "language": self.language_, "content": text_content}
             document = language_v1.ClassifyTextRequest(document=document_dict)
             encoding_type = language_v1.EncodingType.UTF8
 
-            response = client.classify_text(document)
+            response = self.client_.classify_text(document)
             print(response.categories[0].name)
             print(str(int(round(response.categories[0].confidence, 3) * 100)) + "%")
             return [response.categories[0].name, int(round(response.categories[0].confidence, 3) * 100)]
@@ -39,3 +42,7 @@ class websitePredictor:
         for url, metadata in self._meta_dict.items():
             predictions_dict[url] = self._get_prediction(metadata)
         return predictions_dict
+
+if __name__ == '__main__':
+    predictor = websitePredictor({'www.ynet.co.il': translation})
+    predictor.predict()
